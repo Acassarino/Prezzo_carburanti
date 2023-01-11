1468946e490b41a6deb25dbd3b7c797ad4351423
Acassarino <ing.acassarino@gmail.com>
2023-01-11 21:44:28 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -106,8 +106,8 @@ st.write( 'Lavoro a cura di Andrea Cassarino')
 st.write( 'Email:  ing.acassarino@gmail.com')
 
 
-"""html_string = "<img style="display:none"; position:relative; left:-9999999px; src="https://clk.tradedoubler.com/click?p=237081&amp;a=1900897&amp;url=https%3A%2F%2Fwww.ticketone.it%2F" />"
-"""st.markdown(html_string, unsafe_allow_html=True)
+#html_string = "<img style="display:none"; position:relative; left:-9999999px; src="https://clk.tradedoubler.com/click?p=237081&amp;a=1900897&amp;url=https%3A%2F%2Fwww.ticketone.it%2F" />"
+#st.markdown(html_string, unsafe_allow_html=True)
 
 header_html = "<img src='https://clk.tradedoubler.com/click?p=237081&amp;a=1900897&amp;url=https%3A%2F%2Fwww.ticketone.it%2F" />' style='display:none'>"
 
