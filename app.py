4c46c6893a17e3e1257edd36bae2f397ba7f286c
Acassarino <ing.acassarino@gmail.com>
2023-01-11 19:08:58 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from pandas.api.types import (
 )
 
 st.title("Controlla il prezzo aggiornato dei Carburanti")
-st.title(df_aggiornamento)
+#st.title(df_aggiornamento)
 #st.write(
 #    """This app is based on this blog [here](https://blog.streamlit.io/auto-generate-a-dataframe-filtering-ui-in-streamlit-with-filter_dataframe/). 
 #    Can you think of ways to extend it with visuals?
