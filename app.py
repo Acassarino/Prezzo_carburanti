7467864f1c339561ddca0bb18ca81b9be217f698
Acassarino <ing.acassarino@gmail.com>
2023-01-11 19:14:08 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -40,8 +40,8 @@ def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
                 pass
 
         if is_datetime64_any_dtype(df[col]):
-            df[col] = df[col].dt.tz_localize(None)
-            #df[col] = df[col].dt.tz_localize(timezone='Europe/Berlin')
+            #df[col] = df[col].dt.tz_localize(None)
+            df[col] = df[col].dt.tz_localize(timezone='Europe/Berlin')
     modification_container = st.container()
 
     with modification_container:
