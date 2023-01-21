53b018a06e454ff59b2c304ecadba2e13004e099
Acassarino <ing.acassarino@gmail.com>
2023-01-21 20:02:25 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from pandas.api.types import (
 )
 
 st.title("Controlla il prezzo aggiornato dei Carburanti")
-st.title("Aggiornamento del 20-01-2023  14:57")
+st.title("Aggiornamento del 21-01-2023  20:00")
 #st.title(df_aggiornamento)
 #st.write(
 #    """This app is based on this blog [here](https://blog.streamlit.io/auto-generate-a-dataframe-filtering-ui-in-streamlit-with-filter_dataframe/). 
